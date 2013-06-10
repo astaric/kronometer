58f15082c979b1bf859e71841317a02f0328896a
astaric <anze.staric@gmail.com>
2013-06-11 00:14:38 +0200

    Replaced ListView with a Spinner.

--- KronometerApp/Kronometer/src/main/java/net/staric/kronometer/MainActivity.java
+++ KronometerApp/Kronometer/src/main/java/net/staric/kronometer/MainActivity.java
@@ -14,31 +14,48 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
     ArrayList<Contestant> listItems=new ArrayList<Contestant>();
     ArrayAdapter<Contestant> adapter;
 
+    Timer timer;
+    Spinner contestants;
+    TextView countdown;
+
+    class updateCountdown extends TimerTask {
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Calendar now = Calendar.getInstance();
+                    int seconds = now.get(Calendar.SECOND);
+                    countdown.setText(String.format("00:%02d", 29-(seconds % 30)));
+
+                }
+            });
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView contestants = (ListView) findViewById(R.id.contestantsList);
-        adapter=new ContestantAdapter(this,
-                R.layout.contestant_listitem,
-                listItems);
-        contestants.setAdapter(adapter);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
-        final MainActivity xxx = this;
-        contestants.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                xxx.selectPlayer(i);
-            }
-        });
+        adapter = new ContestantAdapter(this, R.layout.contestant_listitem, listItems);
+        //adapter = new ArrayAdapter<Contestant>(this, android.R.layout.simple_list_item_1, listItems);
+        contestants = (Spinner)findViewById(R.id.contestants);
+        contestants.setAdapter(adapter);
+        this.fillContestants();
+
+        countdown = (TextView)findViewById(R.id.countdown);
+        timer = new Timer();
+        timer.schedule(new updateCountdown(), 0, 500);
     }
 
     @Override
@@ -48,18 +65,22 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    int i = 1;
-    public void clickStart(View view) {
-        listItems.add(new Contestant(i++, "Anze", "Staric"));
+    public void fillContestants() {
+        for (int i=0; i<30; i++) {
+            listItems.add(new Contestant(i, "Anze", "Staric"));
+        }
         adapter.notifyDataSetChanged();
-        ListView contestants = (ListView) findViewById(R.id.contestantsList);
-        contestants.smoothScrollToPosition(0);
     }
 
-    public void selectPlayer(int index) {
-        Contestant player = listItems.get(index);
-        player.startTime = new Date();
+    public void clickStart(View view) {
+        int index = contestants.getSelectedItemPosition();
+        Contestant biker = (Contestant)contestants.getSelectedItem();
+        biker.startTime = new Date();
         adapter.notifyDataSetChanged();
+        if (index < contestants.getCount() - 1) {
+            contestants.setSelection(index+1);
+        }
+
     }
 }
 
@@ -79,6 +100,11 @@ class Contestant {
     String getFullName() {
         return String.format("%s %s", this.name, this.surname);
     }
+
+    @Override
+    public String toString() {
+        return getFullName();
+    }
 }
 
 class ContestantAdapter extends ArrayAdapter<Contestant>{
@@ -96,6 +122,15 @@ class ContestantAdapter extends ArrayAdapter<Contestant>{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    public View getCustomView(int position, View convertView, ViewGroup parent) {
         View row = convertView;
         ContestantHolder holder = null;
 
@@ -108,6 +143,7 @@ class ContestantAdapter extends ArrayAdapter<Contestant>{
             holder.txtId = (TextView)row.findViewById(R.id.cid);
             holder.txtName = (TextView)row.findViewById(R.id.name);
             holder.txtStartTime = (TextView)row.findViewById(R.id.startTime);
+            holder.txtSyncStatus = (TextView)row.findViewById(R.id.syncStatus);
 
             row.setTag(holder);
         }
@@ -120,6 +156,7 @@ class ContestantAdapter extends ArrayAdapter<Contestant>{
         holder.txtId.setText(((Integer) contestant.id).toString());
         holder.txtName.setText(contestant.getFullName());
         holder.txtStartTime.setText((contestant.startTime == null) ? "" : contestant.startTime.toString());
+        holder.txtSyncStatus.setText("OK");
 
         return row;
     }
@@ -129,5 +166,6 @@ class ContestantAdapter extends ArrayAdapter<Contestant>{
         TextView txtId;
         TextView txtName;
         TextView txtStartTime;
+        TextView txtSyncStatus;
     }
 }
