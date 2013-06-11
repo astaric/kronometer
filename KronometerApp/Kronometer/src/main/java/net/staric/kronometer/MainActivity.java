b5198e12b17c7e4f73309896bee350760db6e540
astaric <anze.staric@gmail.com>
2013-06-12 00:05:24 +0200

    Added back default database config.

--- KronometerApp/Kronometer/src/main/java/net/staric/kronometer/MainActivity.java
+++ KronometerApp/Kronometer/src/main/java/net/staric/kronometer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -63,6 +64,17 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void fillContestants() {
