5ba4d8a730404f113574c30fe41ea3f50359238a
astaric <anze.staric@gmail.com>
2013-06-12 00:56:34 +0200

    Implemented biker list refresh.

--- KronometerApp/Kronometer/src/main/java/net/staric/kronometer/MainActivity.java
+++ KronometerApp/Kronometer/src/main/java/net/staric/kronometer/MainActivity.java
@@ -1,8 +1,13 @@
 package net.staric.kronometer;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +19,24 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -52,7 +75,7 @@ public class MainActivity extends Activity {
         //adapter = new ArrayAdapter<Contestant>(this, android.R.layout.simple_list_item_1, listItems);
         contestants = (Spinner)findViewById(R.id.contestants);
         contestants.setAdapter(adapter);
-        this.fillContestants();
+        //this.fillContestants();
 
         countdown = (TextView)findViewById(R.id.countdown);
         timer = new Timer();
@@ -72,8 +95,114 @@ public class MainActivity extends Activity {
         switch (item.getItemId()) {
             case R.id.action_settings:
                 return true;
+            case R.id.action_refresh_bikers:
+                updateBikers();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void updateBikers() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadBikerListTask().execute("https://kronometer.herokuapp.com/biker/list");
+        } else {
+            new AlertDialog.Builder(this).setTitle("Ne dela").setMessage(":(").setNeutralButton("Close", null).show();
+        }
+    }
+
+    private class DownloadBikerListTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return (new Communicator()).executeHttpGet(urls[0]);
+            } catch (Exception e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+                jsonArray.length();
+                for(int i=0; i<jsonArray.length(); i++) {
+                    JSONObject biker = jsonArray.getJSONObject(i);
+                    int biker_id = biker.getInt("pk");
+                    String biker_name = biker.getJSONObject("fields").getString("name");
+                    String biker_surname = biker.getJSONObject("fields").getString("surname");
+
+                    Contestant contestant = null;
+                    for (int j=0; j<listItems.size(); j++) {
+                        Contestant candidate = listItems.get(j);
+                        if (candidate.id == biker_id) {
+                            contestant = candidate;
+                            break;
+                        }
+                    }
+                    if (contestant != null) {
+                        contestant.name = biker_name;
+                        contestant.surname = biker_surname;
+                    } else {
+                        contestant = new Contestant(biker_id, biker_name, biker_surname);
+                        listItems.add(contestant);
+                    }
+
+                }
+                adapter.notifyDataSetChanged();
+            } catch (JSONException exc) {
+                System.out.println(exc.getMessage());
+            }
+        }
+    }
+
+    private class Communicator {
+        public String executeHttpGet(String URL) throws Exception
+        {
+            BufferedReader in = null;
+            try
+            {
+                HttpClient client = new DefaultHttpClient();
+
+                client.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "android");
+                HttpGet request = new HttpGet();
+                request.setHeader("Content-Type", "text/plain; charset=utf-8");
+                request.setURI(new URI(URL));
+                HttpResponse response = client.execute(request);
+                in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuffer sb = new StringBuffer("");
+                String line = "";
+
+                String NL = System.getProperty("line.separator");
+                while ((line = in.readLine()) != null)
+                {
+                    sb.append(line + NL);
+                }
+                in.close();
+                String page = sb.toString();
+                //System.out.println(page);
+                return page;
+            }
+            finally
+            {
+                if (in != null)
+                {
+                    try
+                    {
+                        in.close();
+                    }
+                    catch (IOException e)
+                    {
+                        Log.d("BBB", e.toString());
+                    }
+                }
+            }
         }
     }
 
