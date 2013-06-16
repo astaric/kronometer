ff1793af0351f8d754c26d77458f26716a91ab65
astaric <anze.staric@gmail.com>
2013-06-16 15:28:52 +0200

    Heroku django admin fix.

--- kronometer/urls.py
+++ kronometer/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url, include
 
 from kronometer.biker import views
+from kronometer import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,6 +14,8 @@ urlpatterns = patterns('',
     url(r'^category/list', views.category_list, name='category/list'),
     url(r'^category/create', views.category_create, name='category/list'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
     url(r'^', views.results, name='results'),
-
 )
