b1cee0886f9a8039d6566ce47ca110f471ef1d4c
astaric <anze.staric@gmail.com>
2013-09-02 14:32:55 +0200

    Display finish activity in fullscreen when in landscape mode.

--- KronometerApp/Kronometer/src/main/java/net/staric/kronometer/activities/FinishActivity.java
+++ KronometerApp/Kronometer/src/main/java/net/staric/kronometer/activities/FinishActivity.java
@@ -1,7 +1,10 @@
 package net.staric.kronometer.activities;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import net.staric.kronometer.R;
 
@@ -10,6 +13,10 @@ public class FinishActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_finish);
     }
 }
