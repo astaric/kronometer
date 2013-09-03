b8bc9064c02feadf208f570a2e8b83ba65d18068
astaric <anze.staric@gmail.com>
2013-09-03 09:33:04 +0200

    Add swipe to dismiss to contestant list.

--- KronometerApp/Kronometer/src/main/java/net/staric/kronometer/activities/FinishActivity.java
+++ KronometerApp/Kronometer/src/main/java/net/staric/kronometer/activities/FinishActivity.java
@@ -5,10 +5,21 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import net.staric.kronometer.ContestantAdapter;
 import net.staric.kronometer.R;
+import net.staric.kronometer.models.Contestant;
+import net.staric.kronometer.utils.SwipeDismissListViewTouchListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FinishActivity extends Activity {
+
+    private ContestantAdapter contestantsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +29,43 @@ public class FinishActivity extends Activity {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
         setContentView(R.layout.activity_finish);
+
+        ListView contestants = (ListView)findViewById(R.id.contestants);
+        contestantsAdapter = new ContestantAdapter(this,
+                R.layout.listitem_contestant,
+                new ArrayList<Contestant>(Arrays.asList(new Contestant[]{
+                        new Contestant(1, "Janez", "Novak"),
+                        new Contestant(2, "France", "Prešeren"),
+                        new Contestant(3, "France2", "Prešeren"),
+                        new Contestant(4, "France3", "Prešeren"),
+                        new Contestant(5, "France4", "Prešeren"),
+                        new Contestant(6, "France5", "Prešeren"),
+                        new Contestant(7, "France6", "Prešeren"),
+                        new Contestant(8, "France7", "Prešeren"),
+                })));
+        contestants.setAdapter(contestantsAdapter);
+
+        SwipeDismissListViewTouchListener touchListener =
+                new SwipeDismissListViewTouchListener(
+                        contestants,
+                        new SwipeDismissListViewTouchListener.DismissCallbacks() {
+                            @Override
+                            public boolean canDismiss(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+                                    contestantsAdapter.remove(contestantsAdapter.getItem(position));
+                                }
+                                contestantsAdapter.notifyDataSetChanged();
+                            }
+                        });
+        contestants.setOnTouchListener(touchListener);
+        contestants.setOnScrollListener(touchListener.makeScrollListener());
+
+
+
     }
 }
