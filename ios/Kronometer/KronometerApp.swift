fd01630363672fa9f7fe936acdd1cc4cc4b3195d
Anze Staric <anze@naringi.bio>
2023-05-29 10:19:44 +0200

    Add empty ios app

---
+++ ios/Kronometer/KronometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KronometerApp.swift
+//  Kronometer
+//
+//  Created by Anze Staric on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct KronometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
